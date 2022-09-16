8a54deaa5e4a09b2109fbed468b0510be7e69651
dammy27 <shine.sun605@gmail.com>
2022-09-16 05:42:46 -0700

    Multiple mains

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,7 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
-* main - causes an infinite loop
+* main - tests function that prints if integer is positive or negative
 * Return: 0
 */
 
@@ -9,7 +9,7 @@ int main(void)
 {
 int i;
 
-i = 0;
+i = 98;
 positive_or_negative(i);
 
 Return (0);
