70ef78be4df3ff8cb493efc44fb826fc590379b0
dammy27 <shine.sun605@gmail.com>
2022-09-28 08:22:40 -0700

    tasks

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -6,17 +6,17 @@
  * Return: evaluate sqrt
  */
 
-int _evaluate(int 1, int n)
+int _evaluate(int i, int n)
 {
 	/*Evaluate function*/
 	if (n == 0 || n == 1)
 		return (n);
 
-	else if (1 * 1 < n)
+	else if (i * i < n)
 		return (_evaluate(i + 1, n));
 
-	else if (1 * 1 == n) /*Condiction base*/
-		return (1);
+	else if (i * i == n) /*Condiction base*/
+		return (i);
 
 	return (-1);
 
